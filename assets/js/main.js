97b41855078246a8df39770133eb9c1abec81af6
djcurley89 <doug.c.inbox@gmail.com>
2023-06-26 15:45:06 -0700

    2023 big update

--- assets/js/main.js
+++ assets/js/main.js
@@ -241,6 +241,15 @@ video.addEventListener("canplay", function() {
 			.on('load', function() {
 				$window.trigger('resize');
 			});
+	$(document).ready(function() {
+ 
+    setTimeout(function(){
+        $('body').addClass('loaded');
+        $('h1').css('color','#222222');
+    }, 3000);
+ 
+});
+	
 
 
 })(jQuery);
